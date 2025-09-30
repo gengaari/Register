0b26877ee86580ccb312fb61ec1c1004072cea8e
gengaari <chamapham@gmail.com>
2025-09-30 10:51:29 +0300

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Регистрация прошла успешно!";
+                return View("Success");
+            }
+
+            return View(model);
+        }
+
+        public JsonResult CheckUsername(string username)
+        {
+            bool exists = false; 
+            if (exists)
+            {
+                return Json(false);
+            }
+            return Json(true);
+        }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Имя пользователя может содержать только буквы, цифры и подчёркивания.")]
+        [Remote(action: "CheckUsername", controller: "Account", ErrorMessage = "Имя пользователя уже занято.")]
+        public string Username { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Неверный формат email.")]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Range(18, 100, ErrorMessage = "Возраст должен быть от 18 до 100 лет.")]
+        public int Age { get; set; }
+
+        [Url]
+        public string Website { get; set; }
+
+        [CreditCard]
+        public string CreditCardNumber { get; set; }
+
+        [ValidateNever]
+        public bool TermsOfService { get; set; }
+    }
+}
